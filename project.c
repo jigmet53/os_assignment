b681f573093afa26ca2b5560758df3040ae29ebc
jigmet53 <63304206+jigmet53@users.noreply.github.com>
2020-04-09 20:36:54 +0530

    Update project.c

--- project.c
+++ project.c
@@ -1,173 +1,110 @@
+#include<unistd.h>
 #include<stdio.h>
-struct process
-{
-	int p_id;
-	int b_t;
-	int a_t;
-	int prior;
-	int waitTime;
-};
-int total_time,burst_time=0; //to calculate whole complete completion time and burst time
-int total=-1,i=-1; //to calcualte no of process entered in cpu and i to put result in cpu 
-/*to make buffer of queue in which process will be entered 
-and final process after completion to be entered in result of buffer 100;*/
-struct process queue[100],result[100],swap;
-//function to ask number of process to be created by user
-int process_create()
-{
-	int n;
-	printf("enter the number of process you want to enter:");
-	scanf("%d",&n);
-	return n;
+int comt[40]={0};
+int t_q=1;
+int qas[40]={0};
+int c=0;
+int st;
+int btm[40]={0};
+int tt;
+int wt;
+int at[40];
+int bt[40];
+int zz;
+int a;
+void queue1(int fin,int tm);
+void queue2(int fin, int tm);
+void pop(int fin);
+float att, awt;
+int flg=0,tm=0,ee=0,fin=0;
+void pop(int fin){
+qas[ee]=fin+1;
+ee++;
 }
-//function to execute the process and finish it
-void execute()
-{
-	if(total>=0)
-	{
-		int wait,j;
-		//to increase the priority and decrease the burst time of priority in excecution
-		if(burst_time!=0 && queue[0].b_t!=0)
-		{
-			queue[0].b_t--;
-			burst_time--;
-			queue[0].prior++;
-			queue[0].a_t=total_time+1;
-			total_time++;
-		//to increase the wait and priority of waiting process	
-			for(wait=1;wait<=total;wait++)
-			{
-				queue[wait].prior+=2;
-				queue[wait].waitTime=++queue[wait].waitTime;
-			}
-		}
-		//if process gets completed ,it is put in result queue
-		if(queue[0].b_t==0)
-		{
-			i++;
-			result[i]=queue[0];
-			for(wait=0;wait<total;wait++)
-			{
-				queue[wait]=queue[wait+1];
-			}
-			total--;
-		}
-		//to sort the process again in by priority
-		for(wait=0;wait<total;wait++)
-		{
-			for(j=0;j<total;j++)
-			{
-				if(queue[wait].prior<=queue[j].prior)
-				{
-					swap=queue[wait];
-					queue[wait]=queue[j];
-					queue[j]=swap;
-				}
-			}
-		}
-		if(queue[0].prior<=queue[1].prior && total>=1)
-		{
-			swap=queue[0];
-			for(wait=0;wait<total;wait++)
-			{
-				queue[wait]=queue[wait+1];
-			}
-			queue[total]=swap;
-		}
-	}
+void queue1(int fin,int tm){
+for(int x=fin+1;x<4;x++){
+if(at[x]<=tm){
+qas[ee]=x+1;
+ee++;}
 }
+}
+void queue2(int fin, int tm){
+for(int x=fin+1;x<4;x++){
+int fl=0;
+for(int y=0;y<ee;y++){
+if(qas[y]==x+1){
+fl++;}}
+if(at[x]<=tm && fl==0 && btm[x]!=0){
+qas[ee]=x+1;
+ee++;}
+}
+}
+int main()
+{
+printf("-------------------------------------------------welcome------------------------------------------------------\n");
+printf("----------------------------------------------OS Assignment----------------------------------------------------\n");
+do
+{
+printf("Enter no of process  :");
+scanf("%d",&zz);
+for(int x=0;x<zz;x++){
+printf("\n Process %d",x+1);
+printf("\nEnter Arrival Time  :");
+scanf("%d",&at[x]);
+printf(" Enter Burst Time  :");
+scanf("%d",&bt[x]);
+btm[x]=bt[x];}
+do{
+if(flg==0){
+st=at[0];
+if(btm[0]<=t_q){
+tm=st+btm[0];
+btm[0]=0;
+queue1(fin,tm);}
+else{
+btm[0]=btm[0]-t_q;
+tm=st+t_q;
+queue1(fin,tm);
+pop(fin);}
+}
+else{
+fin=qas[0]-1;
+st=tm;
+for(int x=0;x<ee && ee!=1;x++){
+qas[x]=qas[x+1];}
+ee--;
+if(btm[fin]<=t_q){
+tm=st+btm[fin];
+btm[fin]=0;
+queue2(fin, tm);}
+else{
+btm[fin]=btm[fin]-t_q;
+tm=st+t_q;
+queue2(fin, tm);
+pop(fin);}
+}
+if(btm[fin]==0){
+comt[fin]=tm;
+}
+flg++;
+}while(ee!=0);
+printf("}  ");
+printf("\n\nProcess\t Arriv_Time\t Burst_Time\tfinish_Time\tT_a_T\t \tWait_Time\n");
+printf("------------------------------------------------------------------------------\n");
+for(int x=0;x<zz;x++){
+tt=comt[x]-at[x];
+wt=tt-bt[x];
 
-
-/*to give the process id,burst time,arrival time
-and sort the array according to arrival time and if arrival time is equal
-then sort according to burst time in main*/
-void main()
-{
-	int l,j,n=process_create(),count=0;
-	float averageWaitTime=0;
-	struct process pcreate[n];
-	for(l=0;l<n;l++)
-	{
-		pcreate[l].p_id=l+1;
-		printf("\nEnter the arrival time of process[%d]: ",l+1);
-		scanf("%d",&pcreate[l].a_t);
-		printf("\nEnter the burst time of process[%d]: ",l+1);
-		scanf("%d",&pcreate[l].b_t);
-		pcreate[l].prior=0;
-		pcreate[l].waitTime=0;
-		burst_time=burst_time+pcreate[l].b_t;
-	}
-	for(l=0;l<n;l++)
-	{
-		for(j=0;j<n;j++)
-		{
-		if(pcreate[l].a_t<pcreate[j].a_t)
-		{
-			swap=pcreate[l];
-			pcreate[l]=pcreate[j];
-			pcreate[j]=swap;
-		}
-		if(pcreate[l].a_t==pcreate[j].a_t)
-		{
-			if(pcreate[l].b_t<=pcreate[j].b_t)
-			{
-			swap=pcreate[l];
-			pcreate[l]=pcreate[j];
-			pcreate[j]=swap;
-			}
-	 	}
-	    }
-}//	printing the sorted process id with respect to arrival time and if arrival time is equal than burst time.
-	printf("VALUES ENTERED:\n*(TABLE SORTED ACCORDING TO THE AARIVAL TIME)\n\n");
-	printf("               PROCESS TABLE \n");
-	printf("\n.............................................\n");
-	printf(" PROCESS ID           ARRIVAL TIME        SERVICE TIME \n");
-	printf("\n.............................................\n");
-	for(l=0;l<n;l++)
-	{
-		printf(" %d            	   %d                   %d\n",pcreate[l].p_id,pcreate[l].a_t,pcreate[l].b_t );
-	}
-	total_time=pcreate[0].a_t;
-	for(j=pcreate[0].a_t;j<=pcreate[n-1].a_t;j++)
-	{
-		for(l=0;l<n;l++)
-		{
-			if(pcreate[l].a_t==j && count!=n)
-			{
-				total++;
-				queue[total]=pcreate[l];
-				count++;
-			}
-			if(count==n)
-				break;
-		}
-		execute();
-		total_time++;
-	while(burst_time!=0 && count==n)
-	{
-		execute();
-		total_time++;
-	}
-	if(count==n)
-		break;
-	}
-	printf("PROCESS IN ORDER OF THEIR COMPLETION:\n\n");
-	printf("					FINAL PROCESS EXECUTION TABLE \n");
-	printf("................................................................................\n");
-	printf("    PROCESS ID          ARRIVAL TIME             SERVICE TIME          WAITING TIME\n");
-	printf("................................................................................\n");
-	for(l=0;l<n;l++)
-	{
-		for(j=0;j<n;j++)
-		{
-			if(result[l].p_id==pcreate[j].p_id)
-			{
-	printf("  	%d                  %d                    %d                    	%d\n",result[l].p_id,pcreate[j].a_t,pcreate[j].b_t,result[l].waitTime);
-			break;
-			}
-		}
-		averageWaitTime+=(result[l].waitTime);
-	}
-	printf("AVERAGE  WAITING  TIME :%f\n",averageWaitTime/n);
+printf("P%d\t|  %d\t|\t   %d\t|\t  %d\t|\t  %d\t|\t   %d\n",x+1,at[x],bt[x],comt[x],tt,wt);
+printf("------------------------------------------------------------------------------\n");
+awt=awt+wt;
+att=att+tt;
 }
-	
+printf("\nAvg_Turn_Around_Time: %f\nAvg_Waiting_Time: %f",att/4,awt/4);
+
+printf("\nEnter '1' if u want to run again.\n Enter '0' if you want to exit");
+scanf("%d",&a);
+}while(a==1);
+printf("--------------------------------Have great day!!----------------------------");
+printf("\n-------------------------------Thank-You!!----------------------------------");
+}

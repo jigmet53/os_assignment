058dc7b1e8ea7efdadfa82c0b71929959f882d03
jigmet53 <63304206+jigmet53@users.noreply.github.com>
2020-04-09 21:15:36 +0530

    Update project.c

--- project.c
+++ project.c
@@ -1,30 +1,31 @@
 #include<unistd.h>
 #include<stdio.h>
-int comt[40]={0};
-int t_q=1;
-int qas[40]={0};
-int c=0;
-int st;
-int btm[40]={0};
-int tt;
-int wt;
-int at[40];
-int bt[40];
-int zz;
 int a;
+int comt[100]={0};
+int timeq=1;
+int m[100]={0};
+int c=0;
+int k;
+int m1[100]={0};
+int t_a_t;
+int wait_t;
+int ariv_t[100];
+int bur_t[100];
+int zz;
+
 void queue1(int fin,int tm);
 void queue2(int fin, int tm);
 void pop(int fin);
 float att, awt;
 int flg=0,tm=0,ee=0,fin=0;
 void pop(int fin){
-qas[ee]=fin+1;
+m[ee]=fin+1;
 ee++;
 }
 void queue1(int fin,int tm){
 for(int x=fin+1;x<4;x++){
-if(at[x]<=tm){
-qas[ee]=x+1;
+if(ariv_t[x]<=tm){
+m[ee]=x+1;
 ee++;}
 }
 }
@@ -32,10 +33,10 @@ void queue2(int fin, int tm){
 for(int x=fin+1;x<4;x++){
 int fl=0;
 for(int y=0;y<ee;y++){
-if(qas[y]==x+1){
+if(m[y]==x+1){
 fl++;}}
-if(at[x]<=tm && fl==0 && btm[x]!=0){
-qas[ee]=x+1;
+if(ariv_t[x]<=tm && fl==0 && m1[x]!=0){
+m[ee]=x+1;
 ee++;}
 }
 }
@@ -50,40 +51,40 @@ scanf("%d",&zz);
 for(int x=0;x<zz;x++){
 printf("\n Process %d",x+1);
 printf("\nEnter Arrival Time  :");
-scanf("%d",&at[x]);
+scanf("%d",&ariv_t[x]);
 printf(" Enter Burst Time  :");
-scanf("%d",&bt[x]);
-btm[x]=bt[x];}
+scanf("%d",&bur_t[x]);
+m1[x]=bur_t[x];}
 do{
 if(flg==0){
-st=at[0];
-if(btm[0]<=t_q){
-tm=st+btm[0];
-btm[0]=0;
+k=ariv_t[0];
+if(m1[0]<=timeq){
+tm=k+m1[0];
+m1[0]=0;
 queue1(fin,tm);}
 else{
-btm[0]=btm[0]-t_q;
-tm=st+t_q;
+m1[0]=m1[0]-timeq;
+tm=k+timeq;
 queue1(fin,tm);
 pop(fin);}
 }
 else{
-fin=qas[0]-1;
-st=tm;
+fin=m[0]-1;
+k=tm;
 for(int x=0;x<ee && ee!=1;x++){
-qas[x]=qas[x+1];}
+m[x]=m[x+1];}
 ee--;
-if(btm[fin]<=t_q){
-tm=st+btm[fin];
-btm[fin]=0;
+if(m1[fin]<=timeq){
+tm=k+m1[fin];
+m1[fin]=0;
 queue2(fin, tm);}
 else{
-btm[fin]=btm[fin]-t_q;
-tm=st+t_q;
+m1[fin]=m1[fin]-timeq;
+tm=k+timeq;
 queue2(fin, tm);
 pop(fin);}
 }
-if(btm[fin]==0){
+if(m1[fin]==0){
 comt[fin]=tm;
 }
 flg++;
@@ -92,13 +93,13 @@ printf("}  ");
 printf("\n\nProcess\t Arriv_Time\t Burst_Time\tfinish_Time\tT_a_T\t \tWait_Time\n");
 printf("------------------------------------------------------------------------------\n");
 for(int x=0;x<zz;x++){
-tt=comt[x]-at[x];
-wt=tt-bt[x];
+t_a_t=comt[x]-ariv_t[x];
+wait_t=t_a_t-bur_t[x];
 
-printf("P%d\t|  %d\t|\t   %d\t|\t  %d\t|\t  %d\t|\t   %d\n",x+1,at[x],bt[x],comt[x],tt,wt);
+printf("P%d\t|  %d\t|\t   %d\t|\t  %d\t|\t  %d\t|\t   %d\n",x+1,ariv_t[x],bur_t[x],comt[x],t_a_t,wait_t);
 printf("------------------------------------------------------------------------------\n");
-awt=awt+wt;
-att=att+tt;
+awt=awt+wait_t;
+att=att+t_a_t;
 }
 printf("\nAvg_Turn_Around_Time: %f\nAvg_Waiting_Time: %f",att/4,awt/4);
 
